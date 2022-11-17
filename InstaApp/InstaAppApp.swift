3a7eae890810a6680244f01afc63900b33122178
Fabriziochiariello7 <113529227+Fabriziochiariello7@users.noreply.github.com>
2022-11-17 20:59:14 +0100

    Initial Commit

---
+++ InstaApp/InstaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaAppApp.swift
+//  InstaApp
+//
+//  Created by Fabrizio Chiariello on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct InstaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
